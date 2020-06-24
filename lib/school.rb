bce8c69a3cd97b89e6ca1671dd15a7a5e8b5afd8
Mariamol James <mariamol.james@gmail.com>
2020-06-24 03:00:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  attr_accessor :grade, :student, :roster
+  def initialize(school)
+    @school=school
+  end
+  def roster
+    @roster={}
+  end
+  def add_student(student,grade)
+    @school[:grade]=[]
+    @school[:grade]<<student
+  end
+  def grade(grade)
+  end
+end
